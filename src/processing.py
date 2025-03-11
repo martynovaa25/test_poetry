09aa881e7ac2215f5e5684f3d0b943e93cdaf074
Elizaveta Martynova <e_martynovaa@mail.ru>
2025-03-11 23:19:52 +0300

    update processing

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,16 @@
-def filter_by_state(list_input: List[Dict]): -> List[Dict]
-'''функция сортировки по ключу'''
+from typing import List, Dict
+
+
+def filter_by_state(list_input: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+    '''функция сортировки по ключу'''
     new_list = []
     for dictionary in list_input:
-        if i[state] == "EXECUTED":
+        if dictionary[state] == state:
             new_list.append(dictionary)
     return new_list
 
 
-def sort_by_date(list_input: List[Dict], reverse = True): -> List[Dict]
-'''функция сортировки по дате'''
-    sorted_by_date = sorted(list_input, key = lambda x: x['date'], reverse = True)
+def sort_by_date(list_input: List[Dict], reverse: bool = True) -> List[Dict]:
+    '''функция сортировки по дате'''
+    sorted_by_date = sorted(list_input, key = lambda x: x['date'], reverse = reverse)
     return sorted_by_date
-
