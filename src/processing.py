560c7f3a178f50e85ca09305293d9125fea54fe7
Elizaveta Martynova <e_martynovaa@mail.ru>
2025-03-13 12:16:33 +0300

    new small fix readme

--- src/processing.py
+++ src/processing.py
@@ -2,15 +2,15 @@ from typing import List, Dict
 
 
 def filter_by_state(list_input: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
-    '''функция фильтрации по статусу карты'''
+    '''функция сортировки по ключу'''
     new_list = []
     for dictionary in list_input:
-        if dictionary['state'] == state:
+        if dictionary[state] == state:
             new_list.append(dictionary)
     return new_list
 
 
 def sort_by_date(list_input: List[Dict], reverse: bool = True) -> List[Dict]:
-    '''функция сортировки по дате операции'''
+    '''функция сортировки по дате'''
     sorted_by_date = sorted(list_input, key = lambda x: x['date'], reverse = reverse)
     return sorted_by_date
