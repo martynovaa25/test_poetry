8d7ff8b178a3d265b93c3b38b9aa15e5daf78177
Elizaveta Martynova <e_martynovaa@mail.ru>
2025-04-04 18:29:46 +0300

    fix generators

--- generators.py
+++ generators.py
@@ -27,9 +27,10 @@ def transaction_descriptions(transactions):
             yield item["description"]
 
 
-def card_number_generator(number):
+def card_number_generator(start, stop):
     """Генератор, который возвращает номера банковских карт
     в формате XXXX XXXX XXXX XXXX"""
-    card_number = str(number).zfill(16)
-    formatted_card_number = ' '.join([card_number[i:i + 4] for i in range(0, 16, 4)])
-    yield formatted_card_number
+    for number in range(start, stop + 1):
+        card_number = f"{number:016d}"
+        formatted_card_number = " ".join([card_number[i:i + 4] for i in range(0, 16, 4)])
+        yield formatted_card_number
