281636837da7a60e4a458b0854c17cfa498195f9
Elizaveta Martynova <e_martynovaa@mail.ru>
2025-04-04 18:32:28 +0300

    fix generators

--- generators.py
+++ generators.py
@@ -31,6 +31,6 @@ def card_number_generator(start, stop):
     """Генератор, который возвращает номера банковских карт
     в формате XXXX XXXX XXXX XXXX"""
     for number in range(start, stop + 1):
-        card_number = f"{number:016d}"
+        card_number = str(number).zfill(16)
         formatted_card_number = " ".join([card_number[i:i + 4] for i in range(0, 16, 4)])
         yield formatted_card_number
