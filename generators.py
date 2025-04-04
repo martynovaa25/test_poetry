75fbf61eec8a2a24f652b77c5a26b21a6d1b00d3
Elizaveta Martynova <e_martynovaa@mail.ru>
2025-04-04 17:38:19 +0300

    create generators

---
+++ generators.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+@pytest.fixture
+def empty_transactions():
+    return []
+
+
+def filter_by_currency(transaction, currency):
+    """Функция, которая принимает на вход список словарей транзакций и валюту и находит
+    из всего списка транзакцию с нужной валютой"""
+    currency_code = list((x for x in transaction if x["operationAmount"]["currency"]["code"] == currency))
+    for element in currency_code:
+        yield element
+
+
+def transaction_descriptions(transactions):
+    """Генератор, на вход которого подается список словарей с транзакциями,
+    на выходе - описание операции"""
+    if not transactions:
+        raise ValueError("Передано пустое значение!")
+
+    for item in transactions:
+        if "description" not in item:
+            yield 'Отсутствует описание'
+        else:
+            yield item["description"]
+
+
+def card_number_generator(number):
+    """Генератор, который возвращает номера банковских карт
+    в формате XXXX XXXX XXXX XXXX"""
+    card_number = str(number).zfill(16)
+    formatted_card_number = ' '.join([card_number[i:i + 4] for i in range(0, 16, 4)])
+    yield formatted_card_number
