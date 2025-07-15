57f92e8d5a7d639aa7208ac351fde9b4c34b495f
martynovaa25 <martynovaaa.workk@gmail.com>
2025-07-15 16:26:59 +0300

    Revert "Feature/homework 11 2"

--- src/masks.py
+++ src/masks.py
@@ -1,28 +1,23 @@
 from typing import Union
 
+'''получаем данные карты'''
+card_number = input()
+account_number = input()
+
 
 def get_mask_card_number(card_number: Union[str, int]) -> Union[str]:
     '''возвращает скрытый номер карты'''
+    length_number = 16
     str_card_number = str(card_number)
-    if str_card_number == "":
-        return "Пустая строка. Введите номер карты"
-    if not str_card_number.isdigit():
-        return "Введенные данные некорректны"
-
-    if len(str_card_number) != 13 and len(str_card_number) != 16 and len(str_card_number) != 19:
-        return "Номер введен неверно"
-    if len(str_card_number) == 16:
-        return f'{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}'
-    elif len(str_card_number) == 13:
-        return f'{str_card_number[:4]} {str_card_number[4]}**** {str_card_number[-4:]}'
-    elif len(str_card_number) == 19:
-        return f'{str_card_number[:4]} {str_card_number[4:6]}** **** *** {str_card_number[-4:]}'
+    if len(str_card_number) == length_number:
+        return f'{str_card_number[:4]}, {str_card_number[4:6]}**, ****, {str_card_number[-4:]}'
     return ""
 
 
 def get_mask_account(account_number: Union[int, str]) -> Union[str]:
     '''возвращает скрытый номер счета'''
+    length_account = 20
     str_account_number = str(account_number)
-    if len(str_account_number) != 20 or not str_account_number.isdigit():
-        return 'Введенные данные некорректны'
-    return f'**{str_account_number[-4:]}'
+    if len(str_account_number) == length_account:
+        return f'**{str_account_number[-4:]}'
+    return ""
--- src/processing.py
+++ src/processing.py
@@ -5,13 +5,12 @@ def filter_by_state(list_input: List[Dict], state: str = 'EXECUTED') -> List[Dic
     '''функция сортировки по ключу'''
     new_list = []
     for dictionary in list_input:
-        if dictionary.get('state') == state:
+        if dictionary[state] == state:
             new_list.append(dictionary)
-    if not new_list:
-        raise ValueError("Словарь с указанным статусом отсутствует")
+    return new_list
 
 
 def sort_by_date(list_input: List[Dict], reverse: bool = True) -> List[Dict]:
     '''функция сортировки по дате'''
-    sorted_by_date = sorted(list_input, key=lambda x: x['date'], reverse=reverse)
+    sorted_by_date = sorted(list_input, key = lambda x: x['date'], reverse = reverse)
     return sorted_by_date
